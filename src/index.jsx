cfa2b064e36ac340fbe6906efff4e885ba228ec2
Manuel Martinez <martinezrmanuel95@gmail.com>
2022-11-15 19:33:52 -0500

    basic init proyect

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Example from './container/SocialPoster'
+const App=()=>{
+  return(
+    <>
+      <Example></Example>
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(ts|tsx)?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
